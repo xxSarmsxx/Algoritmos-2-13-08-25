71891fbe5d733cd2f5b8f8dc66cf86dce58407eb
xxSarmsxx <11943880712m@gmail.com>
2025-08-28 23:51:57 +0000

    Att do exercício do voo

---
+++ Aula 02/selos.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+
+int main(){
+    
+    int selo3;
+    int selo5;
+    int centavos;
+    int soma;
+    
+    printf("Digite a quantidade de centavos:\n ");
+    scanf("%d", &centavos);
+
+    selo3 = 0;
+    selo5 = 0;
+    soma = 0;
+
+    while (soma < centavos){
+        int resto = centavos - soma;
+
+     if (resto % 5 == 0){
+        selo5 += resto / 5;
+        soma += resto;
+     }
+
+     else {
+        selo3++;
+        soma += 3;
+     }
+    
+    printf("Caso você possua %d centavos, a quantidade de selos de 3 centavos será %d e %d 5 centavos. :)\n", centavos, selo3,selo5);
+    
+    return 0;
+   }
+}
